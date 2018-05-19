31ff393fa709b90772390b01969fe2089fe7b36b
Fabricio Sousa <melapibus@icloud.com>
2018-05-19 01:29:24 -0400

    app.js: Started initMap() and map styles array variable.

--- js/app.js
+++ js/app.js
@@ -10,3 +10,105 @@ var Parks = [
 // Global variables. Empty array for markers and infoWindow variable.
 var markers = [];
 var infoWindow;
+
+// This function allows for the Google Map to be rendered as well as all markers to be created.
+function initMap() {
+
+  // Styles array for Google Maps.
+  var styles = [
+      {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#FFA800"
+              },
+              {
+                  "gamma": 1
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#679714"
+              },
+              {
+                  "saturation": 33.4
+              },
+              {
+                  "lightness": -25.4
+              },
+              {
+                  "gamma": 1
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#53FF00"
+              },
+              {
+                  "saturation": -73
+              },
+              {
+                  "lightness": 40
+              },
+              {
+                  "gamma": 1
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#FBFF00"
+              },
+              {
+                  "gamma": 1
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#00FFFD"
+              },
+              {
+                  "lightness": 30
+              },
+              {
+                  "gamma": 1
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#00BFFF"
+              },
+              {
+                  "saturation": 6
+              },
+              {
+                  "lightness": 8
+              },
+              {
+                  "gamma": 1
+              }
+          ]
+      }
+  ]
+
+}
