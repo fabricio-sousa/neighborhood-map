87ee01ec38a41705ec3955627bf3c942c85a2e41
Fabricio Sousa <melapibus@icloud.com>
2018-05-19 01:18:57 -0400

    app.js: Declared Model Parks array of 5 parks with name and address.

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Model Parks array of 5 parks with name and address.
+var Parks = [
+	{name: 'Simpson Park', address: '5 SW 17th Rd, Miami, FL 33129'},
+	{name: 'Secret Woods', address: '2701 W State Rd 84, Fort Lauderdale, FL 33312'},
+	{name: 'Fern Forest', address: '201 S Lyons Rd, Coconut Creek, FL 33063'},
+	{name: 'Morikami Gardens', address: '4000 Morikami Park Rd, Delray Beach, FL 33446'},
+	{name: 'Quiet Waters Park', address: '401 S Powerline Rd, Deerfield Beach, FL 33442'}
+];
