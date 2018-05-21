8ca7cba6028645bda321035d03b6baab9141f7a6
Fabricio Sousa <melapibus@icloud.com>
2018-05-21 14:10:50 -0400

    app.js: geocordePark(): Added custom icon.

--- js/app.js
+++ js/app.js
@@ -179,7 +179,7 @@ function geocodePark(geocoder, park, parksMap) {
   			animation: google.maps.Animation.DROP,
 				icon: {
 					url: "img/marker/tree.png",
-					scaledSize: new google.maps.Size(64, 64)
+					scaledSize: new google.maps.Size(30, 30)
 				}
 			});
 
